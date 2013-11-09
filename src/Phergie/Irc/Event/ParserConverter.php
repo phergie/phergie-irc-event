e93486b6c929d025726e6834ec03c9886129e128
Matt Frost <mfrost.design@gmail.com>
2013-11-08 23:43:36 -0500

    Creating the ParserConverter and updating Event and EventInterface to include the get/setCommand methods

---
+++ src/Phergie/Irc/Event/ParserConverter.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Phergie (http://phergie.org)
+ *
+ * @link http://github.com/phergie/phergie-irc-bot-react for the canonical source repository
+ * @copyright Copyright (c) 2008-2013 Phergie Development Team (http://phergie.org)
+ * @license http://phergie.org/license New BSD License
+ * @package Phergie\Event
+ */
+
+namespace Phergie\Irc\Event;
+
+/**
+ * ParserConverter converts the output from the parser into a proper object
+ *
+ * @category Phergie
+ * @package Phergie\Irc\Event
+ */
+class ParserConverter
+{
+    /**
+     * Method to convert parser results into an Event or TargetedEvent object
+     *
+     * @param array $parserOutput output from the parser
+     * @return \Phergie\Irc\EventInterface a populated instance of EventInterface
+     */
+    public function convert(array $parserOutput)
+    {
+        if (!empty($parserOutput['targets']) {
+           $event = new TargtedEvent();
+           $event->setTargets($parserOutput['targets']);
+        } else {
+            $event = new Event();
+        }
+        $event->setMessage($parserOutput['message']);
+        $event->setParams($parserOutput['params']);
+        $event->setCommand($parserOutput['command']);
+        return $event;
+    }
+}
+        
